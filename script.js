3bedf772fc7043d55414e830503db01fb3b15a2a
Tamas Gazdik <tamas.gazdik.ext@siemens-healthineers.com>
2022-09-03 18:29:40 +0200

    Playing with variables up to conversion between numbers and strings.

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-changeContent("Content");
-console.log("Hello World right into the console!");
+let loggingMessage = changeContentOfH2("Content of H2");
+console.log(loggingMessage);
+
+let num = 20;
+num = num.toString();
+
+num = Number.parseFloat(`${num}`);
+console.log(`\tValue of number is: ${num}`);
+console.log(`Type of num is ${typeof(num)}`)
+
+console.log((1.1 + 1.3) == 2.4)
--- utils.js
+++ utils.js
@@ -1,4 +1,9 @@
-function changeContent(message)
+function changeContentOfH2(message)
 {
+    let valueBeforeChange = document.getElementById('message').textContent;
     document.getElementById('message').textContent = message;
+    
+    const logMessage = "Content of 'message' was changed";
+
+    return `${logMessage} from '${valueBeforeChange}' to '${document.getElementById('message').textContent}'.` ;
 }
