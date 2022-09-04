d5eb2c912a0961fd9b210174e775489845b585e5
Tamas Gazdik <tamas.gazdik.ext@siemens-healthineers.com>
2022-09-04 15:30:33 +0200

    Added examples up to 'do ... while()' Program Flow.

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-let loggingMessage = changeContentOfH2("Content of H2");
+// example for using a class
+let Lili = new Dog("Lili", 12);
+let loggingMessage = changeContent("h2", Lili);
 console.log(loggingMessage);
 
+// example for using a runtime defined data structure
+let John = {
+    name: "John",
+    age: "35",
+    nationality: "spanish",
+    sex: "male"
+};
+
+let h3_content = `${John.name} is a ${John.age} years old ${John.nationality} ${John.sex}.`;
+changeContent("h3", h3_content);
+
+
+// magic with numbers and strings
 let num = 20;
 num = num.toString();
-
 num = Number.parseFloat(`${num}`);
 console.log(`\tValue of number is: ${num}`);
-console.log(`Type of num is ${typeof(num)}`)
+console.log(`Type of num is ${typeof(num)}`);
+console.log( +(1.1 + 1.3).toFixed(1) === 2.4);
 
-console.log((1.1 + 1.3) == 2.4)
+// magic with numbers #2 and strings
+let n1_1 = "1.1";
+let n1_3 = "1.3";
+console.log(
+    +((+n1_1) + (+n1_3)).toFixed(1) !== 2.4
+);
--- utils.js
+++ utils.js
@@ -1,9 +1,39 @@
-function changeContentOfH2(message)
+function changeContent(elementId, newContent)
 {
-    let valueBeforeChange = document.getElementById('message').textContent;
-    document.getElementById('message').textContent = message;
+    let valueBeforeChange = document.getElementById(elementId)?.textContent;
+    
+    let element = document.getElementById(elementId);
+
+    if( element !== null)
+    {
+        element.textContent = newContent;
+    }
+    else
+    { }
     
     const logMessage = "Content of 'message' was changed";
 
-    return `${logMessage} from '${valueBeforeChange}' to '${document.getElementById('message').textContent}'.` ;
+    return `${logMessage} from '${valueBeforeChange}' to '${document.getElementById(elementId)?.textContent}'.` ;
+}
+
+class Dog
+{
+    name;
+    age;
+
+    constructor(name, age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    toString() {
+        let yearString = this.yearsCalculator(this.age);
+        return `${this.name} the dog is ${this.age} ${yearString} old.`;
+    }
+
+    yearsCalculator(age)
+    {
+        return (age > 1) ? "years" : "year";
+    }
 }
